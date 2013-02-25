eda8a3f0fdec5df2d161a756efc1a284b6106b05
Jim Chen <knh.jabbany@gmail.com>
2013-02-24 23:06:52 -0500

    Implemented PQueue to sort through records of the CacheDB

--- extension/org.kanoha/cachedb.js
+++ extension/org.kanoha/cachedb.js
@@ -99,3 +99,83 @@ function CacheDB(){
 	};
 }
 
+function PQueue(){
+	var pq = [];
+	var size = 0;
+	var compare  = function(a, b){
+		if(a.p > b.p) return 1;
+		else if (a.p < b.p) return -1;
+		return 0;
+	};
+	this.setComparator = function(f){
+		compare = f;
+	};
+	this.insertWithPriority = function(i, p){
+		pq.push({"p":p, "item": i});
+		var idx = pq.length - 1;
+		while(true){
+			if(idx == 0)
+				break;
+			if(compare(pq[idx],pq[Math.floor((idx - 1)/2)]) > 0){
+				var tmp = pq[Math.floor((idx - 1)/2)];
+				pq[Math.floor((idx - 1)/2)] = pq[idx];
+				pq[idx] = tmp;
+				idx = Math.floor((idx - 1)/2);
+			}else{
+				break;
+			}
+		}
+		size ++;
+	};
+	this.poll = function (){
+		if(size == 0)
+			return null;
+		size--;
+		if(size == 0){
+			return pq.pop().item;
+		}
+		var r = pq[0].item;
+		pq[0] = pq.pop();
+		var idx = 0;
+		while(true){
+			if(idx * 2 + 1 >= size)
+				break;
+			if(idx * 2 + 2 >= size){
+				if(compare(pq[idx * 2 + 1], pq[idx]) > 0){
+					var tmp = pq[idx];
+					pq[idx] = pq[idx * 2 + 1];
+					pq[idx * 2 + 1] = tmp;
+					break;
+				}else{
+					break;
+				}
+			}
+			if(compare(pq[idx * 2 + 1], pq[idx * 2 + 2]) > 0){
+				if(compare(pq[idx * 2 + 1], pq[idx]) > 0){
+					var tmp = pq[idx];
+					pq[idx] = pq[idx * 2 + 1];
+					pq[idx * 2 + 1] = tmp;
+					idx = idx * 2 + 1;
+				}else
+					break;
+			}else{
+				if(compare(pq[idx * 2 + 1], pq[idx]) > 0){
+					var tmp = pq[idx];
+					pq[idx] = pq[idx * 2 + 2];
+					pq[idx * 2 + 2] = tmp;
+					idx = idx * 2 + 2;
+				}else
+					break;
+			}
+		}
+		return r;
+	};
+	this.peek = function (){
+		if(pq.length == 0)
+			return null;
+		return pq[0].item;
+	};
+	this.size = function(){
+		return size;
+	}
+}
